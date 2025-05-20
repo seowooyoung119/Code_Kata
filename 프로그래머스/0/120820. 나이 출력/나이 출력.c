1ff49aa7da9da907ab033521acc603ed6fa6e24e
SEOMAN <seohascome20th@naver.com>
2025-05-20 17:52:48 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int unsigned age) {
+    int unsigned answer = 0;
+    
+    answer = 2022 - age+1;
+    
+    
+    return answer;
+    
+}
