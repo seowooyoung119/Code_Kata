a51eb9ac31e90d30d48680390e48d52dffdcd5b8
SEOMAN <seohascome20th@naver.com>
2025-06-14 14:25:03 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.c
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.c
@@ -2,11 +2,19 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int num1, int num2) {
+int solution(int num1, int num2) 
+{
     int answer = 0;
     
-    if(num1 == num2) {answer = 1;}
-    else {answer = -1;} 
-        
+    if(num1 == num2)
+    {
+        answer = 1;    
+    }
+    
+    else if(num1 != num2)
+    {
+        answer = -1;    
+    }
+    
     return answer;
 }
