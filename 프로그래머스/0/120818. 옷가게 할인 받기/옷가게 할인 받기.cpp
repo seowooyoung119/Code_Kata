9bc29f1bcfcf0fbaef01bf882c83defbcd31e197
SEOMAN <seohascome20th@naver.com>
2025-06-15 07:47:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) 
+{
+    
+    if(500000 <= price)return price*0.8;
+    
+    else if(300000 <= price)return price*0.9;
+    
+    else if(100000 <= price)return price*0.95;
+
+    else return price;
+
+}
