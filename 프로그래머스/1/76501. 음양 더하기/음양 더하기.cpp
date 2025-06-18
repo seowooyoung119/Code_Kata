d92b2b15f0d3977556ad567d41eb07c43293267a
SEOMAN <seohascome20th@naver.com>
2025-06-18 09:05:59 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -1,24 +1,10 @@
-#include <string>
 #include <vector>
-
-using namespace std;
-
-int solution(vector<int> absolutes, vector<bool> signs) 
-{    
-    int answer=0;
-    
-    for(int i=0; i<signs.size();++i)
+int solution(std::vector<int> absolutes, std::vector<bool> signs) 
+{
+    auto answer=0;
+    for(int i=0; i < signs.size(); i++)
     {
-        if(false==signs[i])
-        {
-            int num=-1;
-            answer+=(num)*(absolutes[i]);
-            continue;
-        }
-        else
-        {
-        answer+=absolutes[i];
-        }
+        answer += absolutes[i] * (signs[i] ? 1 : -1);
     }
-    return answer;    
+    return answer;
 }
