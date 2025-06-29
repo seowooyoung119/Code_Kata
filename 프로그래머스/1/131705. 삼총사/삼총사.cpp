ec8b610d668f9058e98ae1da7fed780d7115889d
SEOMAN <seohascome20th@naver.com>
2025-06-30 08:58:52 +0900

    [level 1] Title: 삼총사, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.cpp
+++ 프로그래머스/1/131705. 삼총사/삼총사.cpp
@@ -2,16 +2,17 @@
 
 int solution(std::vector<int> number) 
 {
-    int answer = 0;
-    
-    for(int i=0; i<number.size()-2; ++i)
+    int answer=0;
+    int A_NumberSize=number.size()-2;
+    int B_NumberSize=A_NumberSize+1;
+    int C_NumberSize=B_NumberSize+1;
+
+    for(int i=0; i<A_NumberSize; ++i)
     {
-        for(int j=i+1; j<number.size()-1; ++j)
+        for(int j=i+1; j<B_NumberSize; ++j)
         {
-            for(int k=j+1; k<number.size(); ++k)
-            {
-                if(0==number[i]+number[j]+number[k]){++answer;}
-            }
+            int ABPlus=number[i]+number[j];
+            for(int k=j+1; k<C_NumberSize; ++k){if(0==ABPlus+number[k]){++answer;}}
         }
     }
     return answer;
