f2b42b9d5a71becba10f4743cbb34ce3e0c796c1
SEOMAN <seohascome20th@naver.com>
2025-07-02 22:58:12 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.03 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -2,22 +2,27 @@
 
 int solution(std::string t, std::string p) 
 {
-    const int pLen = p.size();
-    const int tLen = t.size();
-    const long long pNum = std::stoll(p);
-
-    // 람다: 주어진 위치에서 p 길이만큼 잘라 숫자로 변환
-    auto getSubNum = [&](int idx) -> long long {return std::stoll(t.substr(idx, pLen));};
-
-    // 람다: 반복 길이 계산
-    auto loopRange = [&]() -> int {return tLen - pLen + 1;};
-
-    // 인라인 스타일 람다: 조건 비교
-    auto isValid = [&](int idx) -> bool {return getSubNum(idx) <= pNum;};
-
-    // 최종 집계
-    int count = 0;
-    for (int i = 0; i < loopRange(); ++i)if (isValid(i)) count++;
+    int pLen = p.size(), tLen = t.size(), answer = 0;
+    long long pNum = 0, num = 0, pow10 = 1;
     
-    return count;
+    for (char ch : p) 
+    {
+        pNum = pNum * 10 + (ch - '0');
+    }
+
+    for (int i = 0; i < pLen; ++i) 
+    {
+        num = num * 10 + (t[i] - '0');
+        if (i < pLen - 1) pow10 *= 10;
+    }
+
+    if (num <= pNum) ++answer;
+
+    for (int i = pLen; i < tLen; ++i) 
+    {
+        num = (num - (t[i - pLen] - '0') * pow10) * 10 + (t[i] - '0');
+        if (num <= pNum) ++answer;
+    }
+    
+    return answer;
 }
