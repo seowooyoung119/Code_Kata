83a88e5b97f3c389dcc20091a39657107ba32de7
SEOMAN <seohascome20th@naver.com>
2025-07-10 19:13:50 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.29 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/README.md
+++ 프로그래머스/1/134240. 푸드 파이트 대회/README.md
@@ -1,10 +1,10 @@
 # [level 1] 푸드 파이트 대회 - 134240 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/134240?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.28 ms
+메모리: 4.14 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 10일 09:27:56
+2025년 07월 10일 19:13:45
 
 ### 문제 설명
 
--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,32 +1,34 @@
-
-#include <string>
 #include <vector>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
-string solution(vector<int> food)
+string solution(vector<int> food) 
 {
     string answer = "";
-    string Water = to_string(0);
-    string First_Half = "";
-    string Second_Half = "";
-
-    for (int i = 1; i < food.size(); ++i)
+    string Half_Food ="";
+    string All_Food = "";
+    
+    for(int i = 1; i < food.size(); ++i)
     {
-        int Food = (food[i] / 2);
-                    
-        for (int j = Food; j > 0; --j)
+        int Food_Count = food[i] / 2;
+        
+        for(int j = 0; j < Food_Count; ++j)
         {
-            First_Half.append(to_string(i));
-        }
+            Half_Food.append(to_string(i));
+        }        
     }
-
-    Second_Half = First_Half;
-
-    reverse(Second_Half.begin(), Second_Half.end());
-
-    answer = (First_Half + Water + Second_Half);
-
+    
+    All_Food = Half_Food;
+  
+    reverse(Half_Food.begin(), Half_Food.end());
+    
+    All_Food += '0';
+        
+    All_Food += Half_Food;
+    
+    answer = All_Food;
+    
     return answer;
 }
