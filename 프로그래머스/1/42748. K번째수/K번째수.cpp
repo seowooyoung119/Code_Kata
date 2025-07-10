6908eda05acb4cb12b4f28eb30e9086860355524
SEOMAN <seohascome20th@naver.com>
2025-07-10 19:39:03 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) 
-{     
+{
     vector<int> answer;
     
     for(auto command : commands)
     {
-        int i = (command[0] - 1); 
-        int j = (command[1] - 1);  
-        int k = (command[2] - 1); 
+        int i = command[0]-1;
+        int j = command[1];
+        int k = command[2]-1;
+    
+        vector<int> Make_Array(array.begin()+i,array.begin()+j);
         
-        vector<int> Make_array (array.begin()+i, array.begin()+j+1);
+        sort(Make_Array.begin(), Make_Array.end());
         
-        sort(Make_array.begin(), Make_array.end());
-        
-        answer.push_back(Make_array[k]);
+        answer.push_back(Make_Array[k]);
     }
     return answer;
 }
