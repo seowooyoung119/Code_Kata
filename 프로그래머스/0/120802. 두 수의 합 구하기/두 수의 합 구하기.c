91415fc9f63e5649887e7812f47ac45063384d12
SEOMAN <seohascome20th@naver.com>
2025-07-24 15:31:29 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.c
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.c
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int num1, int num2) {
-    int answer = num1 + num2;
-    
-    return answer;
+int solution(int num1, int num2) 
+{
+    return num1+num2;
 }
