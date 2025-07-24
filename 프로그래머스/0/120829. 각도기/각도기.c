59382a16f800b94e45d9c40e3282a2a6d0510e1c
SEOMAN <seohascome20th@naver.com>
2025-07-24 15:35:23 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.c
+++ 프로그래머스/0/120829. 각도기/각도기.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int solution(int angle) {
-    int answer = 0;
-    
-     if(angle > 0 && angle < 90)    {answer = 1;}
-     if(90 == angle)                {answer = 2;}
-     if(angle > 90 && angle < 180)  {answer = 3;}
-     if(180 == angle)               {answer = 4;}
-    
-     return answer;
+int solution(int angle) 
+{
+    if(90 == angle) 
+    {
+        return 2;
+    }
+    else if(180 == angle) 
+    {
+        return 4;
+    }   
+    else if(0 < angle && angle < 90)
+    {
+        return 1;
+    }
+    else
+    {
+        return 3;
+    }
+        
 }
