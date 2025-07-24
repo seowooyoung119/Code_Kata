a88ebc97ab777d195cbcb3aa77850f991878a256
SEOMAN <seohascome20th@naver.com>
2025-07-24 15:26:03 +0900

    [level 0] Title: 두 수의 차 구하기, Time: 0.01 ms, Memory: 4.54 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.c
+++ 프로그래머스/0/120803. 두 수의 차 구하기/두 수의 차 구하기.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int solution(int num1, int num2) {
-    int answer = num1 - num2;
-    return answer;
+int solution(int num1, int num2) 
+{
+    return num1 - num2;
 }
