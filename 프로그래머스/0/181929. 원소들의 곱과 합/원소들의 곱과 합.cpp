a814e02f0dfd1c7af91febdf66cacc0eee8c75f7
SEOMAN <seohascome20th@naver.com>
2025-07-26 16:50:15 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -1,21 +1,14 @@
 #include <vector>
+#include <numeric>
+#include <functional>
+#include <cmath>
 
-using std::vector;
+using namespace std;
 
 int solution(vector<int> num_list) 
 {
-    int sum =1, plus=0;
-    for(auto num : num_list)
-    {
-        sum *= num;
-        plus += num;
-    }
-    if(sum < (plus*plus))
-    {
-        return 1;
-    }
-    else if(sum > (plus*plus))
-    {
-        return 0;
-    }
+    int mult = accumulate(num_list.begin(),num_list.end(),1,multiplies<int>());
+    int plus = accumulate(num_list.begin(),num_list.end(),0);
+    
+    return mult < pow(plus,2) ? 1 : 0;  
 }
