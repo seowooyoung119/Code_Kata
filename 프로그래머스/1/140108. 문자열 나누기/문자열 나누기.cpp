36e9eff7a95ec048b2ebba32edc93ecf910ba6b7
SEOMAN <seohascome20th@naver.com>
2025-07-26 18:01:05 +0900

    [level 1] Title: 문자열 나누기, Time: 0.04 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -5,26 +5,38 @@ using namespace std;
 
 int solution(string s) 
 {
-    int count = 0;
+    // 첫 글자부터 읽기
+    // 첫 글자 == 다음 글자 비교
+    // 같으면 같다고 ++ , 다르면 다르고 ++
+    // 위의 조건이 같아지면 break;
+    // 그 후 count 업
+    // 모든 조건 리셋 -> 비교한 글자 인덱스 위치 다음 문자부터 -> 루프 반복
+    // 마지막 글자까지 가면 while 종료
+    
+    int Count = 0;
     int i = 0;
     
     while(i < s.length())
-    {              
-        char a = s[i];
-        int a_num = 0;
-        int other_num = 0;
-   
+    {
+        char ComChar = s[i];
+        int Count_Equal = 0;
+        int Count_Other = 0;
+        
         for(int j = i; j < s.length(); ++j)
         {
-            if(a == s[j]) ++a_num;
-            else ++other_num;
-            if(a_num == other_num) {i = j+1; break;}
+            if(ComChar == s[j]) ++Count_Equal;
+            else ++Count_Other;
+            
+            if(Count_Equal == Count_Other) 
+            {
+                i = j+1;
+                break;
+            }   
         }
         
-        if (a_num != other_num) i = s.length();
-            
-        ++count;
+        if(Count_Equal != Count_Other) {i = s.length(); ++Count; break;}
+        ++Count;
     }
     
-    return count;
+    return Count;
 }
