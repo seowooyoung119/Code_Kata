58ed00894b13ad09976fb655ec523d967679ce2c
SEOMAN <seohascome20th@naver.com>
2025-09-15 00:18:41 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+int solution(vector<int> citations) 
+{
+    sort(citations.begin(), citations.end(), greater<int>());
+    
+    int hIndex = 0;
+    for (int i = 0; i < citations.size(); i++)
+    {
+        if (citations[i] >= i + 1)
+        {
+            hIndex = i + 1;
+        }
+        else 
+        {
+            break; 
+        }
+    }
+    return hIndex;
+}
